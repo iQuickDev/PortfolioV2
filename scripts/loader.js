e8d9c3420b0cdd637607dc5ad4e6584f370df203
iQuickGaming <iquickpublic@gmail.com>
2021-12-25 02:43:02 +0100

    skybox

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,10 +1,27 @@
-function updatePercentage()
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    })
+  }
+
+async function updatePercentage()
 {
     let count = 0
     let loader = document.querySelector("#loader")
     let percentage = document.querySelector("#percentage")
+    let loadingContent = document.querySelector("#description")
 
-    let percentageUpdater = setInterval(() =>
+    let loadingText = document.querySelector("#description")
+    let loadingTextUpdater = setInterval(() =>
+    {
+        loadingText.textContent += "."
+        if (loadingText.textContent.length > 18)
+        {
+            loadingText.textContent = "Loading Content"
+        }
+    }, 250)
+
+    let percentageUpdater = setInterval(async () =>
     {
         count++
         percentage.textContent = `${count}%`
@@ -12,10 +29,13 @@ function updatePercentage()
         if (count == 100)
         {
             clearInterval(percentageUpdater)
+            clearInterval(loadingTextUpdater)
             loader.classList.add("fadeout")
+            loadingContent.textContent = "DONE!"
+            await sleep(1000)
+            loader.remove()
         }
     }, 25)
-
 }
 
 updatePercentage()
---
+++ scripts/main.js
@@ -0,0 +1,64 @@
+import * as THREE from 'https://threejs.org/build/three.module.js';
+import {OrbitControls} from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+
+function main()
+{
+  const canvas = document.querySelector('#context');
+  const renderer = new THREE.WebGLRenderer({canvas});
+
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = .1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 0, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  
+    const loader = new THREE.CubeTextureLoader().setPath("../media/images/background/");
+    const texture = loader.load([
+      'galaxyPosX.png',
+      'galaxyNegX.png',
+      'galaxyPosY.png',
+      'galaxyNegY.png',
+      'galaxyPosZ.png',
+      'galaxyNegZ.png',
+    ])
+
+    scene.background = texture;
+
+  function resizeRendererToDisplaySize(renderer)
+  {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function render(time) {
+    time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer))
+    {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+main();
